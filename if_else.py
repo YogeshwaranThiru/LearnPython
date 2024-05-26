af5032683c52cba0bdfcf0e54f077561f652daf3
YogeshwaranThiru <yogeshwaranthirunavukkarasu@gmail.com>
2024-05-26 22:34:02 +0530

    If_else file

---
+++ if_else.py
@@ -0,0 +1,8 @@
+age = int(input("Please provide your age: "))
+
+if age >= 18: 
+    print("you are adult")
+elif age >= 11 and age <= 19:
+    print ("you are a teenager")
+else:
+    print("you are a kid")
